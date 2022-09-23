2b6bcb359a0a4eb0351978be22741b26ba11051f
Rian Febriansyah <rianfebriansyah22@gmail.com>
2022-09-23 10:00:16 +0700

    feat: finish onboarding layout & logic

---
+++ data/onboard.js
@@ -0,0 +1,14 @@
+export const onboarding = [
+    {
+        title: "Temukan teman belajar lebih mudah",
+        desc: "Ketika kamu mengalami penurunan motivasi belajar, kamu bisa mencari teman belajarmu di sini untuk mengembalikan semangat belajarmu.",
+    },
+    {
+        title: "Temukan berdasarkan bidang/minat",
+        desc: "Ketika kamu mengalami penurunan motivasi belajar, kamu bisa mencari teman belajarmu di sini untuk mengembalikan semangat belajarmu.",
+    },
+    {
+        title: "Temukan teman belajar di sekitarmu",
+        desc: "Ketika kamu mengalami penurunan motivasi belajar, kamu bisa mencari teman belajarmu di sini untuk mengembalikan semangat belajarmu.",
+    },
+];
